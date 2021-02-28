ef462e182d92526933e9754a351aa35ffa39ed50
ManiyamGT <79772011+ManiyamGT@users.noreply.github.com>
2021-02-27 20:07:18 -0500

    Initial setup

---
+++ src/main/java/com/mgt/app/bankaccount/transaction/notification/listener/BankAccountTransactionNotificationListenerApplication.java
@@ -0,0 +1,13 @@
+package com.mgt.app.bankaccount.transaction.notification.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountTransactionNotificationListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountTransactionNotificationListenerApplication.class, args);
+	}
+
+}
