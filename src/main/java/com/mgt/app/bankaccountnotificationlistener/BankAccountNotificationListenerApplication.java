230aa3dee4e1878b9e71f9c6f91ae69e4e840412
ManiyamGT <79772011+ManiyamGT@users.noreply.github.com>
2021-02-27 19:22:58 -0500

    FirstCommit
    Base Setup

---
+++ src/main/java/com/mgt/app/bankaccountnotificationlistener/BankAccountNotificationListenerApplication.java
@@ -0,0 +1,13 @@
+package com.mgt.app.bankaccountnotificationlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountNotificationListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountNotificationListenerApplication.class, args);
+	}
+
+}
